88dd61a95a6c82094322287c5deff0369c97f6e0
RapDog64 <20298329+RapDog64@users.noreply.github.com>
2020-09-15 17:37:34 +0300

    Make some changes

--- BDDAutotests/Utility/DataProvider.cs
+++ BDDAutotests/Utility/DataProvider.cs
@@ -5,9 +5,9 @@ namespace BDDAutotests.Utility
     public class DataProvider
     {
 
-        private readonly User user = new User("ivanvasya9681@yandex.ru", "ivanqwerty2");
-        private readonly Message message = new Message("denis152324@mail.ru", "denis152324", "messsage to test", "Hello this is a test message.");
-        private readonly LetterForm form = new LetterForm("IvanVasya9681", "Перейти в папку «Входящие»", "Войти в почту");
+        private readonly User user = new User("", "");
+        private readonly Message message = new Message("", "", "messsage to test", "Hello this is a test message.");
+        private readonly LetterForm form = new LetterForm("", "Перейти в папку «Входящие»", "Войти в почту");
 
         public User User => user;
 
